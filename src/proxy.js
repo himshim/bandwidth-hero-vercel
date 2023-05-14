6a8570bb13b7ae592f7e2c16c017644232706880
himshim26 <97538806+himshim@users.noreply.github.com>
2023-05-14 07:15:20 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,43 +1,47 @@
-const request = require('request')
-const pick = require('lodash').pick
-const shouldCompress = require('./shouldCompress')
-const redirect = require('./redirect')
-const compress = require('./compress')
-const bypass = require('./bypass')
-const copyHeaders = require('./copyHeaders')
+const axios = require('axios');
+const { pick } = require('lodash');
+const shouldCompress = require('./shouldCompress');
+const redirect = require('./redirect');
+const compress = require('./compress');
+const bypass = require('./bypass');
+const copyHeaders = require('./copyHeaders');
 
-function proxy(req, res) {
-  request.get(
-    req.params.url,
-    {
-      headers: {
-        ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-        'user-agent': 'Bandwidth-Hero Compressor',
-        'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        via: '1.1 bandwidth-hero'
-      },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) return redirect(req, res)
+async function proxy(req, res) {
+  const url = req.params.url;
+  const headers = {
+    cookie: req.headers.cookie,
+    dnt: req.headers.dnt,
+    referer: req.headers.referer,
+    'user-agent': 'Bandwidth-Hero Compressor',
+    'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
+    via: '1.1 bandwidth-hero',
+  };
+  const config = {
+    timeout: 10000,
+    maxRedirects: 5,
+    responseType: 'arraybuffer',
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+  };
 
-      copyHeaders(origin, res)
-      res.setHeader('content-encoding', 'identity')
-      req.params.originType = origin.headers['content-type'] || ''
-      req.params.originSize = buffer.length
-
-      if (shouldCompress(req)) {
-        compress(req, res, buffer)
-      } else {
-        bypass(req, res, buffer)
-      }
+  try {
+    const response = await axios.get(url, { headers, ...config });
+    if (response.status >= 400) {
+      return redirect(req, res);
     }
-  )
+
+    copyHeaders(response.headers, res);
+    res.setHeader('content-encoding', 'identity');
+    req.params.originType = response.headers['content-type'] || '';
+    req.params.originSize = response.data.length;
+
+    if (shouldCompress(req)) {
+      compress(req, res, response.data);
+    } else {
+      bypass(req, res, response.data);
+    }
+  } catch (error) {
+    redirect(req, res);
+  }
 }
 
-module.exports = proxy
+module.exports = proxy;
