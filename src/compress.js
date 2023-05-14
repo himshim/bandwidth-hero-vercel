1cf989234047b940fee94f5e54f3b8ef63d832dc
himshim26 <97538806+himshim@users.noreply.github.com>
2023-05-14 07:14:18 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,27 +1,35 @@
-const sharp = require('sharp')
-const redirect = require('./redirect')
+const sharp = require('sharp');
+const cache = require('node-cache'); // or any other caching library
+const redirect = require('./redirect');
 
-function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg'
+const imageCache = new cache({ stdTTL: 60 * 60 * 24 }); // cache TTL set to one day
 
-  sharp(input)
-    .grayscale(req.params.grayscale)
-    .toFormat(format, {
+async function compress(req, res, input) {
+  const format = req.params.webp ? 'webp' : 'jpeg';
+  const key = `${input}:${format}:${req.params.grayscale}:${req.params.quality}`;
+
+  let compressedImageBuffer = imageCache.get(key);
+
+  if (!compressedImageBuffer) {
+    const image = sharp(input).grayscale(req.params.grayscale);
+    image.toFormat(format, {
       quality: req.params.quality,
       progressive: true,
-      optimizeScans: true
-    })
-    .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) return redirect(req, res)
+      optimizeScans: true,
+    });
 
-      res.setHeader('content-type', `image/${format}`)
-      res.setHeader('content-length', info.size)
-      res.setHeader('x-original-size', req.params.originSize)
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size)
-      res.status(200)
-      res.write(output)
-      res.end()
-    })
+    compressedImageBuffer = await image.toBuffer();
+    imageCache.set(key, compressedImageBuffer);
+  }
+
+  res.setHeader('content-type', `image/${format}`);
+  res.setHeader('content-length', compressedImageBuffer.length);
+  res.setHeader('x-original-size', req.params.originSize);
+  res.setHeader(
+    'x-bytes-saved',
+    req.params.originSize - compressedImageBuffer.length
+  );
+  res.status(200);
+  res.write(compressedImageBuffer);
+  res.end();
 }
-
-module.exports = compress
